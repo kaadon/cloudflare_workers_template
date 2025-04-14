399c1b2444f32ee51a3de3090d0c707d5327572f
kaadon <kaadon.com@gmail.com>
2025-04-14 13:45:59 +0800

    commit

--- src/utils/route.js
+++ src/utils/route.js
@@ -4,21 +4,48 @@ class Route {
 	}
 
 	get(path, handler) {
-		this.routes.push({ method: "GET", path: new RegExp(`^${path}$`), handler });
+		this.routes.push({ method: 'GET', path: new RegExp(`^${path}$`), handler });
 	}
 
 	post(path, handler) {
-		this.routes.push({ method: "POST", path: new RegExp(`^${path}$`), handler });
+		this.routes.push({ method: 'POST', path: new RegExp(`^${path}$`), handler });
+	}
+
+
+	handleResult(data) {
+		const result = {
+			code: 200,
+			message: 'success',
+			data: data
+		};
+		return new Response(JSON.stringify(result), { status: 200 });
+	}
+
+	handleError(error) {
+		const result = {
+			code: 201,
+			message: 'error',
+			error: error?.message || 'error'
+		};
+		return new Response(JSON.stringify(result), { status: 201 });
+	}
+
+	handleNotFound() {
+		const result = {
+			code: 500,
+			message: 'path not found'
+		};
+		return new Response(JSON.stringify(result), { status: 500 });
 	}
 
 	async handle(ctx) {
 		try {
 			const url = new URL(ctx.request.url);
 			const route = this.routes.find(route => route.method === ctx.request.method && route.path.test(url.pathname));
-			if(!route) {
+			if (!route) {
 				return this.handleNotFound(ctx);
 			}
-			if(route.method === "GET") return route.handler(ctx);
+			if (route.method === 'GET') return route.handler(ctx);
 			const data = await route.handler(ctx);
 			return this.handleResult(data);
 		} catch (error) {
@@ -26,31 +53,6 @@ class Route {
 		}
 	}
 
-	 handleResult(data) {
-		const result = {
-            code: 200,
-            message: 'success',
-            data: data
-        }
-      return new Response(JSON.stringify(result), { status: 200 });
-	}
-
-	 handleError(error) {
-		const result = {
-            code: 201,
-            message: 'error',
-            error: error?.message || "error"
-        }
-      return new Response(JSON.stringify(result), { status: 201 });
-	}
-
-	 handleNotFound() {
-		const result = {
-            code: 500,
-            message: 'path not found'
-        }
-        return new Response(JSON.stringify(result), { status: 500 });
-	}
 }
 
 export default Route;
